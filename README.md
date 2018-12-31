b9511fe81372241b29153fffbf5b4618897653bb
赵思阳 <zhaosiyang@s-MacBook-puro.local>
2018-12-31 18:09:25 -0500

    first version finish

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Cookie Observable
+Read cookie as an observable
+
+### Install
+```npm install cookie-observable```
+
+### Use
+```javascript
+getCookieObservable().subscribe(cookie => console.log('cookie'));
+```
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {Observable, BehaviorSubject} from 'rxjs';
+
+export function getCookieObservable(): Observable<string> {
+    const key = 'cookie';
+    const target = Object.getPrototypeOf(Object.getPrototypeOf(document));
+    const setter = Object.getOwnPropertyDescriptor(target, key).set;
+    const subject = new BehaviorSubject<string>(document.cookie);
+    Object.defineProperty(target, key, {
+        set: value => {
+            setter.call(document, value);
+            subject.next(document.cookie);
+        }
+    });
+    return subject.asObservable();
+}
+
+
+
